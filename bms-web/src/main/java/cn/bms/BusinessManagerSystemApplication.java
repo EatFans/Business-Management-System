94ab32d8f67ad908b2d791e5b622c343ddf43bdb
EatFan <eatfan0921@163.com>
2025-03-30 10:39:21 +0800

    init

---
+++ bms-web/src/main/java/cn/bms/BusinessManagerSystemApplication.java
@@ -0,0 +1,16 @@
+package cn.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动程序
+ * @author Fan
+ */
+@SpringBootApplication
+public class BusinessManagerSystemApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BusinessManagerSystemApplication.class,args);
+
+    }
+}
