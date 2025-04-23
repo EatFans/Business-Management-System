e214ecd9b05a58296104da29c647b96757166287
EatFan <eatfan0921@163.com>
2025-04-23 20:34:15 +0800

    后端完成PermissionService基本的增删查改的业务逻辑

--- bms-system/src/main/java/cn/bms/system/service/PermissionService.java
+++ bms-system/src/main/java/cn/bms/system/service/PermissionService.java
@@ -1,9 +1,32 @@
 package cn.bms.system.service;
 
+import cn.bms.domain.entity.Permission;
+
 /**
  * 权限业务接口类
  *
  * @author Fan
  */
 public interface PermissionService {
+
+    /**
+     * 通过权限id获取权限信息
+     * @param id id
+     * @return 权限信息
+     */
+    Permission selectPermissionById(Long id);
+
+    /**
+     * 添加权限
+     * @param permission 权限信息
+     * @return 如果添加成功就返回true，否则就返回false
+     */
+    boolean addPermission(Permission permission);
+
+    /**
+     * 更新权限
+     * @param permission 权限信息
+     * @return 如果更新成功就返回true，否则就返回false
+     */
+    boolean updatePermission(Permission permission);
 }
--- bms-system/src/main/java/cn/bms/system/service/impl/PermissionServiceImpl.java
+++ bms-system/src/main/java/cn/bms/system/service/impl/PermissionServiceImpl.java
@@ -1,6 +1,9 @@
 package cn.bms.system.service.impl;
 
+import cn.bms.domain.entity.Permission;
+import cn.bms.system.mapper.PermissionMapper;
 import cn.bms.system.service.PermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,7 +14,42 @@ import org.springframework.stereotype.Service;
 @Service
 public class PermissionServiceImpl implements PermissionService {
 
-    public PermissionServiceImpl(){
+    @Autowired
+    private PermissionMapper permissionMapper;
 
+    /**
+     * 通过权限id获取权限信息
+     * @param id id
+     * @return 权限信息
+     */
+    @Override
+    public Permission selectPermissionById(Long id) {
+        if (id != null && id != 0){
+            return permissionMapper.selectPermissionById(id);
+        }
+        return null;
+    }
+
+    /**
+     * 添加权限
+     * @param permission 权限信息
+     * @return 如果成功添加就返回true，否则就返回false
+     */
+    @Override
+    public boolean addPermission(Permission permission) {
+        int flag = permissionMapper.insertPermission(permission);
+        return flag > 0;
+    }
+
+    /**
+     * 更新权限
+     * @param permission 权限信息
+     * @return 如果成功更新就返回true，否则就返回false
+     */
+    @Override
+    public boolean updatePermission(Permission permission) {
+        int flag = permissionMapper.updatePermission(permission);
+        return flag > 0;
     }
 }
+
