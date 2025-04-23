6728b893d9663450c842b7b1cd12b51305d35ccb
EatFan <eatfan0921@163.com>
2025-04-23 20:33:48 +0800

    后端完成PermissionMapper基本的增删查改接口方法

--- bms-system/src/main/java/cn/bms/system/mapper/PermissionMapper.java
+++ bms-system/src/main/java/cn/bms/system/mapper/PermissionMapper.java
@@ -1,5 +1,6 @@
 package cn.bms.system.mapper;
 
+import cn.bms.domain.entity.Permission;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
@@ -10,5 +11,24 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface PermissionMapper {
 
+    /**
+     * 通过权限id来筛选获取权限
+     * @param id id
+     * @return 权限信息
+     */
+    Permission selectPermissionById(Long id);
 
+    /**
+     * 新增一条权限数据
+     * @param permission 权限信息
+     * @return 结果
+     */
+    int insertPermission(Permission permission);
+
+    /**
+     * 更新权限信息
+     * @param permission 权限信息
+     * @return 结果
+     */
+    int updatePermission(Permission permission);
 }
